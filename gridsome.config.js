1c5b006ec6df0adc1cc9ba274c42c078f1704b4d
Marcus Battle <themarcusbattle@gmail.com>
2019-11-05 15:58:16 -0500

    Added Google Analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,15 +7,22 @@
 const tailwindcss = require("tailwindcss")
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: [],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: [
-          tailwindcss
-        ],
-      },
-    },
-  }
+  	siteName: 'Men Pray - Life skills men\'s conference | Greensboro, NC',
+	plugins: [
+		{
+			use: '@gridsome/plugin-google-analytics',
+			options: {
+				id: 'UA-151586436-1'
+			}
+		}
+	],
+	css: {
+		loaderOptions: {
+			postcss: {
+				plugins: [
+					tailwindcss
+				],
+			},
+		},
+	}
 }
